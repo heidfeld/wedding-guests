ec53856786e11cb9aa040d7424bb17c6ebe963dd
heidfeld <l.karmanski@gmail.com>
2020-07-13 08:45:57 +0200

    fix #3 + added jest/enzyme libraries and basic test

---
+++ src/utils/__tests__/KonvaHelpers.test.js
@@ -0,0 +1,9 @@
+import {getFileName} from "../KonvaHelpers";
+
+describe("TEST KonvaHelpers", () => {
+
+    it('check getFileName', () => {
+        const fileName1 = getFileName('fileName', 'pdf');
+    });
+
+});
